61166fb9a0e30e9718b15ad0ceb3aad3c0649f68
tavenharris <tavenharris@gmail.com>
2024-10-05 22:48:06 -0700

    Use environment variables for Firebase configuration
    This improves security by preventing sensitive information from being hard-coded into the application. Now, the configuration values are pulled from environment variables, which can be managed more securely.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const config = {
-   apiKey: "AIzaSyBhQRfzanT9sX0FTfiw1b4SLW1lsUl9QJQ",
-   authDomain: "scu-theta-tau-website.firebaseapp.com",
-   projectId: "scu-theta-tau-website",
-   storageBucket: "scu-theta-tau-website.appspot.com",
-   messagingSenderId: "126559078877",
-   appId: "1:126559078877:web:853dfd16e3ba197e7c4e34"
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(config);
