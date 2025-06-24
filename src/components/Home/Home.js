082bdb9d8383afde71cb0b5e6f5d61852eb97cc6
tavenharris <tavenharris@gmail.com>
2025-06-24 11:28:22 -0700

    Updated Regent Section to show Xander's message from the regent on Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import backgroundImage from '../assets/HomePage_Hero.png'; // Replace with the correct image path
-import regentImage from '../assets/regent.jpg'; // Replace with the correct image path
+import regentImage from '../assets/regent.jpeg'; // Replace with the correct image path
 // import thetaClass from '../assets/ThetaClassInitiation.png'; // Import the image here
 import EtaClass from '../assets/EtaClassPic.jpeg'; // Import the image here
 
@@ -62,31 +62,21 @@ const Home = () => {
                     <img src={regentImage} alt="Ryan Kiniris" className="regent-image"/>
                     <div className="regent-text">
                         <p>
-                            Hello!<br/><br/>
-                            My name is Ryan Kiniris, and I'm currently a junior majoring in Electrical and Computer
-                            Engineering in the Eta class at SCU. Theta Tau has been an integral part of my college
-                            journey, offering an exceptional community that blends professional growth with lifelong
-                            friendships.<br/><br/>
-                            Theta Tau stands out as a fraternity where excellence is nurtured in every aspect. Our
-                            members are not only dedicated to their academic and professional achievements, but they
-                            also bring a vibrant, engaging spirit to everything we do. Being part of such a dynamic
-                            group has been incredibly rewarding. The bonds I've formed here are with some of the most
-                            genuine and supportive individuals I've ever met, and they’ve helped shape both my personal
-                            and academic life in profound ways.<br/><br/>
-                            Choosing to join Theta Tau has undeniably been one of the best decisions I've made at SCU.
-                            This fraternity has given me countless opportunities for growth, leadership, and building a
-                            network of remarkable peers who inspire me every day. The experiences and memories I've
-                            gained here are irreplaceable, and I am continuously grateful for the sense of belonging and
-                            camaraderie that defines our brotherhood.<br/><br/>
-                            Thank you for taking the time to get to know us a little better! I hope you'll consider
-                            joining us for our Winter Rush during Week 1. It’s a perfect chance to experience the unique
-                            spirit and supportive environment that makes Theta Tau so special.<br/><br/>
+                            Hi!<br/><br/>
+                            My name is Xander Fruin, and I'm currently a senior at SCU majoring in Mechanical Engineering with a minor in Aerospace Engineering, part of the Theta class. Theta Tau has been integral to my college journey, where I’ve been able to grow professionally and meet some of my closest friends. In my free time, I enjoy spirited driving, hiking, photography, and videography.<br/><br/>
+
+                            Theta Tau stands out as a fraternity where excellence is nurtured in every aspect. Our members are not only dedicated to their academic and professional achievements, but they also bring a vibrant, engaging spirit to everything we do. Being part of such a dynamic group has been incredibly rewarding. The bonds I've formed here are with some of the most genuine and supportive individuals I've ever met, and they’ve helped shape both my personal and academic life in profound ways.<br/><br/>
+
+                            Choosing to join Theta Tau has undeniably been one of the best decisions I've made at SCU. This fraternity has given me countless opportunities for growth, leadership, and building a network of remarkable peers who inspire me every day. The experiences and memories I've gained here are irreplaceable, and I am continuously grateful for the sense of belonging and camaraderie that defines our brotherhood.<br/><br/>
+
+                            Thank you for taking the time to get to know us a little better, and if you ever see me around campus, feel free to say hi! I hope you'll consider joining us for our Winter Rush during Week 1. It’s a perfect chance to experience the unique spirit and supportive environment that makes Theta Tau so special.<br/><br/>
+
                             In H & T,<br/><br/>
-                            RYAN KINIRIS<br/>
-                            ELECTRICAL AND COMPUTER ENGINEERING, ETA CLASS<br/>
-                            JUNIOR | SCU THETA TAU
+                            XANDER FRUIN<br/>
+                            MECHANICAL & AEROSPACE ENGINEERING, THETA CLASS<br/>
+                            SENIOR | SCU THETA TAU
                         </p>
-                        <p className="regent-signature">Ryan Kiniris | Regent</p>
+                        <p className="regent-signature">Xander Fruin | Regent</p>
                     </div>
                 </div>
             </div>
