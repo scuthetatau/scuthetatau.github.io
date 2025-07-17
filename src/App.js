93cc6391d0f0173a8e0d1d42a59a1a2831f83ea1
tavenharris <tavenharris@gmail.com>
2025-07-16 22:47:10 -0700

    Refactor App.js to improve formatting, add Vercel Speed Insights, and enable debug mode for Analytics.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
 import MeetTheBrothers from './components/MeetTheBrothers/MeetTheBrothers';
@@ -17,37 +17,41 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import ScribeEditor from "./components/ScribeEditor/ScribeEditor";
 // import BroDatesPage from './components/Brodates/BroDatesPage';
 
-import { Analytics } from "@vercel/analytics/react"
+// Vercel Imports
+import {Analytics} from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
         <Router>
             <div>
-                <Header />
+                <Header/>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about-us" element={<AboutUs />} />
-                    <Route path="/meet-the-brothers" element={<MeetTheBrothers />} />
-                    <Route path="/rush" element={<Rush />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/admin/user-management" element={<UserManagement />} />
-                    <Route path="/admin/bro-dates" element={<ManageBroDates />} />
-                    <Route path="/admin/spoon-assassins" element={<SpoonAssassins />} />
-                    <Route path="/alumni" element={<Alumni />} />
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about-us" element={<AboutUs/>}/>
+                    <Route path="/meet-the-brothers" element={<MeetTheBrothers/>}/>
+                    <Route path="/rush" element={<Rush/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/admin" element={<Admin/>}/>
+                    <Route path="/admin/user-management" element={<UserManagement/>}/>
+                    <Route path="/admin/bro-dates" element={<ManageBroDates/>}/>
+                    <Route path="/admin/spoon-assassins" element={<SpoonAssassins/>}/>
+                    <Route path="/alumni" element={<Alumni/>}/>
 
                     <Route path="/dashboard" element={<Dashboard/>}/>
 
                     <Route path="/scribe-editor" element={<ScribeEditor/>}/>
                     <Route path="/family-tree" element={<FamilyTree/>}/>
 
-                    {/*<Route path="/bro-dates" element={<BroDatesPage />} />*/}
+                    <Route path="/bro-dates" element={<BroDatesPage/>}/>
                 </Routes>
             </div>
             <footer className="footer">
                 <p>&copy; {new Date().getFullYear()} Upsilon Epsilon Chapter of Theta Tau. All rights reserved.</p>
             </footer>
-            <Analytics />
+            <SpeedInsights/>
+            <Analytics debug={true}/>
+
         </Router>
     );
 }
